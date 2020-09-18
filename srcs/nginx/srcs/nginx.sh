370403257b086fd752b3b34f0933b9ddcc31e497
MikelTolino <tolino20@gmail.com>
2020-09-18 02:22:44 +0200

    Basic Nginx Configuration

--- srcs/nginx/srcs/nginx.sh
+++ srcs/nginx/srcs/nginx.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-apk update && apk add nginx openrc --no-cache
+apk update
+apk add nginx openrc openssl --no-cache
 adduser -D -g 'www' www
-mkdir /www && chown -R www:www /var/lib/nginx
+mkdir /www
+chown -R www:www /var/lib/nginx
 chown -R www:www /www
-mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.orig
 mv /tmp/nginx.conf /etc/nginx/
 mv /tmp/index.html /www/
+mkdir -p /run/nginx
+yes "" | openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/certs/localhost.key -out /etc/ssl/certs/localhost.crt
 openrc
 touch /run/openrc/softlevel
+rc-service nginx start
